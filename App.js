fcee28ac5fce4b76fbbe4d76733c51ab966f7f06
kalyanitirumalasetty <146421175+kalyanitirumalasetty@users.noreply.github.com>
2024-12-26 15:11:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Crud from "./components/crud/crudjs1.js";
+
+function App() {
+  return (
+    <div>
+     
+     <Crud></Crud>
+    </div>
+   
+  );
+}
+
+export default App;
